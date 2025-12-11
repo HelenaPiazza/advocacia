4c46b57bdd4eeb5b6da98309f8642385971e78e5
HelenaPiazza <hpiazza.souza@gmail.com>
2025-12-11 07:56:30 -0300

    comentarios

--- js/animacoes.js
+++ js/animacoes.js
@@ -3,16 +3,20 @@ const elementosAnimados = document.querySelectorAll('.animar-scroll');
 
 // Função que verifica quais elementos estão visíveis na tela
 const verificarVisibilidade = () => {
-    const alturaJanela = window.innerHeight * 0.8; // 80% da altura da tela
+    //varifica se a altura da tela é igual a 80%
+    const alturaJanela = window.innerHeight * 0.8; 
 
+    // Inicia um loop que verifica individualmente cada item que deve ser animado.
     elementosAnimados.forEach((elemento) => {
+        // Calcula a distância  do topo do elemento até o topo da tela.
         const topoElemento = elemento.getBoundingClientRect().top;
 
         // Se o topo do elemento estiver dentro dos 80% da tela
         if (topoElemento < alturaJanela) {
+            //adiciona a classe visivel ao elemento
             elemento.classList.add('visivel');
         } else {
-            // Opcional: remover a classe se sair da tela (para animar de novo)
+            // Remover a classe se sair da tela (para animar de novo)
             //elemento.classList.remove('visivel');
         }
     });
